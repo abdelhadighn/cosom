007b80bccc85171437a351ea6bd6025baf76e1dd
Abdelhadi Ghn <118575384+abdelhadighn@users.noreply.github.com>
2025-05-02 21:44:05 +0100

    button white

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -19,7 +19,7 @@ export function About() {
             nous sélectionnons avec soin chaque produit pour garantir fraîcheur et qualité.
           </p>
           <Link to="/products">
-            <Button variant="consomOutline" size="lg" className="text-consom border-white hover:bg-white border-consom ">
+            <Button variant="consomOutline" size="lg" className="text-consom border-white hover:bg-white hover:border-consom text-consom ">
               Explorer le catalogue <ArrowRight className="ml-2" />
             </Button>
           </Link>
